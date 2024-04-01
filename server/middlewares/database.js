3c38a199ef017408848e98b4ae59d24755c7d383
Osman Karadeniz <osmankaradeniz2000@gmail.com>
2024-04-01 09:31:02 +0300

    database configuration

--- server/middlewares/database.js
+++ server/middlewares/database.js
@@ -1,10 +1,11 @@
 const Sequelize = require("sequelize");
 const db = new Sequelize(
-    'todo_db',
+    'todo_db_6kue',
     'ronesans2',
-    '1234',
+    'reWiR9kct248ujXC040qrw3cRC90gwNs',
     {
-        host: 'localhost',
+        host: 'dpg-co556o4f7o1s7398740g-a',
+	port: 5432,
         dialect: 'postgres'
     }
 );
